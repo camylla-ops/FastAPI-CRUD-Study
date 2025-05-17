94b5d8020a52638bd3d0c1f5df0202d871dff8b8
camylla <camylla.oliveiraps@gmail.com>
2025-05-17 20:01:19 -0300

    att

--- app/main.py
+++ app/main.py
@@ -30,13 +30,7 @@ def read_item(item_id: int):
         raise HTTPException(status_code=404, detail="Item não encontrado")
     return items[item_id]
 
-#define a route to read all items
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    if item_id not in items:
-        raise HTTPException(status_code=404, detail="Item não encontrado")
-    items[item_id] = item
-    return {"message": "Item atualizado com sucesso", "item": item}
+
 
 #define a route to delete an item
 @app.delete("/items/{item_id}")
@@ -46,7 +40,10 @@ def delete_item(item_id: int):
     del items[item_id]
     return {"message": "Item deletado com sucesso"}   
 
-# Define a route to read all items       
-@app.get("/items")
-def read_all_items():
-    return items    
+#define a route to read all items
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    if item_id not in items:
+        raise HTTPException(status_code=404, detail="Item não encontrado")
+    items[item_id] = item
+    return {"message": "Item atualizado com sucesso", "item": item}
